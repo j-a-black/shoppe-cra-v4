ea1b452d60d1ca140e53d0aed580cb12fca9d808
jasonanthonblack@gmail.com <jasonanthonblack@gmail.com>
2022-05-05 20:18:28 -0500

    styling hero component

--- src/components/hero/hero.component.jsx
+++ src/components/hero/hero.component.jsx
@@ -2,12 +2,15 @@ import "./hero.styles.scss";
 
 const Hero = () => {
   return (
-    <section>
-      <h1>
-        <span>Exquisite Coffee & Desserts</span>
-        <span>For People with Exquisite Taste</span>
-      </h1>
-    </section>
+    <div className="hero-container grid-center-item">
+      <section className="hero-text-box">
+        <h1>
+          <span className="header-primary">Carli's Shoppe</span>
+          <span className="header-sub">Exquisite Coffee & Desserts</span>
+          <span className="header-sub">For People with Exquisite Taste</span>
+        </h1>
+      </section>
+    </div>
   );
 };
 
