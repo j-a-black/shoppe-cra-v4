52e5a04c54acfb5daddc3a61261ca64abb635f38
jasonanthonblack@gmail.com <jasonanthonblack@gmail.com>
2022-05-09 18:32:26 -0500

    started FormInput component

--- src/components/form/form.component.jsx
+++ src/components/form/form.component.jsx
@@ -1,9 +1,27 @@
+import FormInput from "../form-input/form-input.component";
 import "./form.styles.scss";
 
 const Form = () => {
   return (
-    <div>
-      <h1>This is the Form Component</h1>
+    <div className="form">
+      <h3>Sign Up for our Monthly Newsletter!</h3>
+      <p>Join our mailing list to stay up-to-date on our latest specials!</p>
+      <form>
+        <FormInput
+          label="name"
+          type="text"
+          className="form-input"
+          required
+          name="name"
+        />
+        <FormInput
+          label="email"
+          type="email"
+          className="form-input"
+          required
+          name="email"
+        />
+      </form>
     </div>
   );
 };
